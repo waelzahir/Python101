98f282636fa94aee303564c0d2fe0092a050d8e5
waelzahir <ozahir@student.1337.ma>
2024-02-06 02:41:34 +0100

    init

---
+++ 02DetectTypes/find_ft_type.py
@@ -0,0 +1,10 @@
+def all_thing_is_obj(object: any) -> int:
+        if object is None  :
+                return 
+        
+        print(object)
+        # types = type(object)
+        # print(type)
+        return 42
+
+
